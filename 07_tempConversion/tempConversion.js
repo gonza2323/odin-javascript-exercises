c015899971e4d57a33e3dfff76d16bba029f3802
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-12-22 00:43:31 -0300

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  return Number.parseFloat(((F - 32) * 100/180).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  return Number.parseFloat((C * 180/100 + 32).toFixed(1));
 };
 
 // Do not edit below this line
