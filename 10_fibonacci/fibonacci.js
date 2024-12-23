997df3102c60c1255075c2b4110daa6c494722fd
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-12-23 02:48:23 -0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if (n < 0) return "OOPS";
+    
+    if (typeof countArg !== 'number')
+        n = parseInt(n);
+
+    let n2 = 1;
+    let n1 = 0;
+    let result = 0;
+    let i = 0;
+
+    while (i < n) {
+        result = n2 + n1;
+        n2 = n1;
+        n1 = result;
+        i++;
+    }
+    return result;
 };
 
 // Do not edit below this line
