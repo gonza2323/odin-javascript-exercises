00e0f50ce881dc2a24de87680ed613845b402018
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2024-12-23 02:26:59 -0300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const filteredWord = word.toLowerCase()
+                        .split('')
+                        .filter(l => ('a' <= l && l <= 'z') || ("0" <= l && l <= "9"));
+    
+    return filteredWord.join('') === filteredWord.reverse().join('');
 };
 
 // Do not edit below this line
